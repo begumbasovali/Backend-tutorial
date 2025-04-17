9862c5e3198520bce09404ab3f3fbd4652959f1f
begumbasovali <begum.basovali@gmail.com>
2025-04-17 13:19:53 +0300

    use superheroes package

--- 2-NPM/index.js
+++ 2-NPM/index.js
@@ -1,4 +1,10 @@
+//Use sillyname package to generate a random silly name
 //var generateName = require("sillyname");
-import generateName from "sillyname";
-var sillyname = generateName();
-console.log(`My silly name is ${sillyname}.`);
+//import generateName from "sillyname";
+//var sillyname = generateName();
+//console.log(`My silly name is ${sillyname}.`);
+
+//Use superheroes package to generate a random superhero name
+import {randomSuperhero} from 'superheroes';
+const name = randomSuperhero();
+console.log(`I am ${name}.`);
