e4728f0b4c82db60cc5f52eae78edf712d7e27d6
begumbasovali <begum.basovali@gmail.com>
2025-04-17 13:12:32 +0300

    use ESM rather than CJS

--- 2-NPM/index.js
+++ 2-NPM/index.js
@@ -1,4 +1,4 @@
-var generateName = require("sillyname");
-
+//var generateName = require("sillyname");
+import generateName from "sillyname";
 var sillyname = generateName();
 console.log(`My silly name is ${sillyname}.`);
