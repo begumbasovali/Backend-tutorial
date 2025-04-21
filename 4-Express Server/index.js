c74c2650ff6437285f9536c6c868621884a42b7e
begumbasovali <begum.basovali@gmail.com>
2025-04-21 13:19:27 +0300

    solution to cannot GET

--- 4-Express Server/index.js
+++ 4-Express Server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 
 const app = express(); //create an instance of express
+
+app.get("/", (req,res) => {
+  res.send("Hello World!"); //send a response to the client
+});
+
 const port = 3000;
 app.listen(port ,() => { //port number and callback function
     console.log(`Server is running on port ${port}`); 
